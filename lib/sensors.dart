bcbb96465fac230807ba5deca960b4b2fd54bf53
ALberto <albertocarlosmartin@gmail.com>
2021-02-04 16:21:21 -0500

    added sensor class

---
+++ lib/sensors.dart
@@ -0,0 +1,24 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Sensors extends StatefulWidget {
+  @override
+  _SensorsState createState() => _SensorsState();
+}
+
+class _SensorsState extends State<Sensors> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+
+  }
+}
